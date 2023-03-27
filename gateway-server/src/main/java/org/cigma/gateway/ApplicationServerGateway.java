59c6a8192ca532b64c346cb08fe4b807b20033db
BAHAFIDabdessadek <abdessadek.bahafid@gmail.com>
2023-03-27 16:02:22 +0000

    gateway serve

---
+++ gateway-server/src/main/java/org/cigma/gateway/ApplicationServerGateway.java
@@ -0,0 +1,12 @@
+package org.cigma.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationServerGateway {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationServerGateway.class,args);
+    }
+}
